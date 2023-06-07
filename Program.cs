65e7a493d5a28123edb3a12f44d0c93be213c29e
Yair Sadan <yairsadan1@gmail.com>
2023-06-07 13:37:45 +0300

    my quad tree

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ tree.Add(new Box() { Id = 2, Width = 3, Height = 1 });
 tree.Add(new Box() { Id = 3, Width = 1, Height = 3 });
 tree.Add(new Box() { Id = 4, Width = 3, Height = 3 });
 
-var box = tree.Search(new Box() { Width = 9, Height = 9 });
-
+Box desired = tree.Search(new Box() { Width = 3, Height = 2 }, 100);
 Console.ReadLine();
 
--- QuadTree.cs
+++ QuadTree.cs
@@ -6,6 +6,7 @@ public class Quadtree<T> where T : Box
     public Quadtree<T> Ghgw { get; set; } // Greater height and greater width
     public Quadtree<T> Ghsw { get; set; } // greater height and smaller width
 
+    public T FoundValue { get; set; }
     public bool Add(T valueToAdd)
     {
         // Case Smaller height and smaller width
@@ -58,45 +59,84 @@ public class Quadtree<T> where T : Box
         else return false;
     }
     private int _foundValueId;
-    public T Search(T valueToSearch)
+
+    public T Search(T valueToSearch, int maxDiffernce)
     {
-        // Case equal to value
-        if (valueToSearch == Value) return Value;
+        privateSearch(valueToSearch, maxDiffernce);
+        return FoundValue;
+    }
+    private bool privateSearch(T valueToSearch, int maxDiffernce)
+    {
+        // Case it meets the criteria
+        if (IsInTheCriteria(Value, valueToSearch, maxDiffernce))
+        {
+            FoundValue = Value;
+            return true;
+        }
 
         // Case Smaller height and smaller width
         else if (valueToSearch.Height <= Value.Height && valueToSearch.Width <= Value.Width)
         {
-            if (Shsw == null)
-                return this.Value;
+            // Checks whether we have a smaller unit on height and with
+            if (Shsw != null && Shsw.privateSearch(valueToSearch, maxDiffernce))
+                return true;
+
+            // Checks whether we have a smaller unit on height but with a greater width
+            else if (Shgw != null && Shgw.privateSearch(valueToSearch, maxDiffernce))
+                return true;
             else
-                return Shsw.Search(valueToSearch);
+                return false;
         }
 
         // Case Smaller height and greater width
         else if (valueToSearch.Height <= Value.Height && valueToSearch.Width > Value.Width)
         {
-            if (Shgw == null)
-                return this.Value;
+            if (Ghsw != null && Ghsw.privateSearch(valueToSearch, maxDiffernce))
+                return true;
+            else if (Ghgw != null && Ghgw.privateSearch(valueToSearch, maxDiffernce))
+                return true;
             else
-                return Shgw.Search(valueToSearch);
+                return false;
         }
 
         // Case Greater height and smaller width
         else if (valueToSearch.Height > Value.Height && valueToSearch.Width <= Value.Width)
         {
-            if (Ghsw == null)
-                return this.Value;
+            if (Shgw != null && Shgw.privateSearch(valueToSearch, maxDiffernce))
+                return true;
+            else if (Ghgw != null && Ghgw.privateSearch(valueToSearch, maxDiffernce))
+                return true;
             else
-                return Ghsw.Search(valueToSearch);
+                return false;
         }
+
         // Case Greater height and greater width
-        else if (Value.Height <= valueToSearch.Height && valueToSearch.Width <= valueToSearch.Width)
+        else if (valueToSearch.Height > Value.Height && valueToSearch.Width > Value.Width)
         {
-            if (Ghgw == null)
-                return this.Value;
+            if (Shsw != null && Shsw.privateSearch(valueToSearch, maxDiffernce))
+                return true;
+            else if (Shgw != null && Shgw.privateSearch(valueToSearch, maxDiffernce))
+                return true;
+            else if (Ghsw != null && Ghsw.privateSearch(valueToSearch, maxDiffernce))
+                return true;
             else
-                return Ghgw.Search(valueToSearch);
+                return false;
         }
-        else return null;
+        else return false;
+    }
+
+
+    // Check whether the value meets the user differece criteria e.g. if the user criteria is 25% and the size he want is 5cm on 5cm 
+    // the given box can be minmum of 5cm on 5cm and maximum of 6.25cm on 6.25cm (5 * 125%)
+    private bool IsInTheCriteria(T Value, T ValueToCheck, int maxDiffernce)
+    {
+        // checks whether it's larger
+        if (Value.Height >= ValueToCheck.Height && Value.Width >= ValueToCheck.Width)
+        {
+            // checks whether it's int the criteria
+            if (ValueToCheck.Height * (1 + maxDiffernce / 100) >= Value.Height && ValueToCheck.Width * (1 + maxDiffernce / 100) >= Value.Width)
+                return true;
+        }
+        return false;
     }
 }
